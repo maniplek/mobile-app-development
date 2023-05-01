33c861bf350bdeeba3462dca4e93ce9f6b01c1c2
Maniple kwizera <maniple.kwizera@codeland.it>
2023-05-01 08:31:35 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>WELCOME</Text>
+      <Text>Let's Hunt Native !!</Text>
+      <Button title='Tap me'/>
     </View>
   );
 }
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems:'center',
     justifyContent: 'center',
   },
 });
