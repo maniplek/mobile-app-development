ced4d3ad11bdffcb1f7eec3639358a48ca70077b
Maniple kwizera <maniple.kwizera@codeland.it>
2023-05-02 23:41:09 +0300

    added flexbox

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>WELCOME</Text>
-      <Text>Let's Hunt Native !!</Text>
-      <Button title='Tap me'/>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput  style={styles.textInput} placeholder="Your Course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of Goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems:'center',
-    justifyContent: 'center',
+  appContainer:{
+    paddingTop:50,
+    paddingHorizontal:16
   },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center",
+  },textInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'70%',
+    marginRight: 8,
+    padding: 8,
+  }
 });
