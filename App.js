be139415db8bfe0304ef4b76a1a7fd516bfe4d74
Maniple kwizera <maniple.kwizera@codeland.it>
2023-05-03 23:30:56 +0300

    addded new styles

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-          <Text style={styles.dummyText}>WELCOME</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput  style={styles.textInput} placeholder="Your Course goal" />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Let's Hunt Native !!</Text>
-      <Button title='Tap me'/>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems:'center',
-    justifyContent: 'center',
+  appContainer:{
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16
   },
-  dummyText:{
-    margin:16,
-    padding: 15,
-    borderWidth:2,
-    borderColor:'gray',
+  inputContainer:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor: '#cccccc',
+  },textInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer:{
+    flex: 5,
   }
 });
