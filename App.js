90a98243317576927a918a33e2c59cc7e728b8f6
Maniple kwizera <maniple.kwizera@codeland.it>
2023-05-03 23:23:21 +0300

    added styles

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         <TextInput  style={styles.textInput} placeholder="Your Course goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals....</Text>
       </View>
     </View>
@@ -18,18 +18,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
+    flex:1,
     paddingTop:50,
     paddingHorizontal:16
   },
   inputContainer:{
+    flex:1,
     flexDirection: 'row',
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor: '#cccccc',
   },textInput:{
     borderWidth:1,
     borderColor: '#cccccc',
     width:'70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer:{
+    flex: 5,
   }
 });
