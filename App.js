47928f997939b5f056196e40c300c62b01d045cd
Maniple kwizera <maniple.kwizera@codeland.it>
2023-05-04 23:37:45 +0300

    added event handler

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput  style={styles.textInput} placeholder="Your Course goal" />
-        <Button title="Add Goal" />
+        <TextInput  
+        style={styles.textInput} 
+        placeholder="Your Course goal" 
+        onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of Goals....</Text>
